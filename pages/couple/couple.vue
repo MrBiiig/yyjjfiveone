8e8c99b69cdd001ff5dd138d193a405fd230a9eb
MrBig <457965950@qq.com>
2025-03-06 20:13:50 +0800

    九宫格

--- pages/couple/couple.vue
+++ pages/couple/couple.vue
@@ -1,43 +1,34 @@
 <template>
-	<view id="container">
-		<view class="box">
-			<view class="box-img">
-				<image src="https://gd-hbimg.huaban.com/ede93cf241ce93df1f824022ce1bc6dd042879819162e-fvCJH6" alt="" />
-			</view>
-		</view>
-		<view class="box">
-			<view class="box-img">
-				<image src="https://gd-hbimg.huaban.com/2d14622d76dbcd036cc47f240c80d5f1b9e6481da3430-r5cTIt" alt="" />
-			</view>
-		</view>
-		<view class="box">
-			<view class="box-img">
-				<image src="https://gd-hbimg.huaban.com/824306750839c34939b4345039e7425740598dd510a9c1-ewh29L" alt="" />
-			</view>
-		</view>
-		<view class="box">
-			<view class="box-img">
-				<image src="https://gd-hbimg.huaban.com/824306750839c34939b4345039e7425740598dd510a9c1-ewh29L" alt="" />
-			</view>
-		</view>
-		<view class="box">
-			<view class="box-img">
-				<image src="https://gd-hbimg.huaban.com/ce90e5b734b0c9933335d962a02ae6e8227965b6a984c-Gg1vRz" alt="" />
-			</view>
-		</view>
-		<view class="box">
-			<view class="box-img">
-				<image src="https://gd-hbimg.huaban.com/9fa5103953d1b3f2a70984f17395c1cf1eddc8db2af38-Q2fm1x" alt="" />
-			</view>
-		</view>
-		<view class="box">
-			<view class="box-img">
-				<image src="https://gd-hbimg.huaban.com/ce90e5b734b0c9933335d962a02ae6e8227965b6a984c-Gg1vRz" alt="" />
-			</view>
-		</view>
-	</view>
+  <view>
+		<uni-grid :column="2" :highlight="true" :showBorder="false"  @change="change">
+				<uni-grid-item v-for="(item, index) in images" :index="index" :key="index">
+					<view class="grid-item-box" style="background-color: #cccccc;width: 100%;height: 100%;padding: 8rpx;">
+						<image style="width: 100%;height: 100%;" mode="aspectFill" :src="item"></image>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+  </view>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
 
-<style></style>
+const images = [
+  "https://gd-hbimg.huaban.com/ede93cf241ce93df1f824022ce1bc6dd042879819162e-fvCJH6",
+  "https://gd-hbimg.huaban.com/2d14622d76dbcd036cc47f240c80d5f1b9e6481da3430-r5cTIt",
+  "https://gd-hbimg.huaban.com/824306750839c34939b4345039e7425740598dd510a9c1-ewh29L",
+  "https://gd-hbimg.huaban.com/824306750839c34939b4345039e7425740598dd510a9c1-ewh29L",
+  "https://gd-hbimg.huaban.com/ce90e5b734b0c9933335d962a02ae6e8227965b6a984c-Gg1vRz",
+  "https://gd-hbimg.huaban.com/9fa5103953d1b3f2a70984f17395c1cf1eddc8db2af38-Q2fm1x",
+  "https://gd-hbimg.huaban.com/ce90e5b734b0c9933335d962a02ae6e8227965b6a984c-Gg1vRz"
+];
+
+
+onMounted(() => {
+
+});
+
+</script>
+
+<style>
+</style>
